eb2c470a04c6a7262e5f78037aedc142dda886d2
Annie Carrillo <anacarrillo49@aragon.unam.mx>
2022-08-22 07:25:33 -0500

    Commit Inicio

---
+++ Ejercicio_Nodos/Nodo.py
@@ -0,0 +1,14 @@
+
+from multiprocessing import Value
+from optparse import Values
+from turtle import left, right
+from typing import ValuesView
+
+class NodoTernario:
+
+    def __init__( self , Values , left= None, center= None rigth= None):
+    self.data = ValuesView
+    self.left = left
+    self.center = center
+    self.rigth = rigth
+
